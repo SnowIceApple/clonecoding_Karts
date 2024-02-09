99d338a1278f8317a0a45d9408cd1453884193f1
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-02-09 21:30:46 +0900

    dd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+    $('.hr_util > a').each(function(){
+        $(this).on('click', function(){
+            $(this).parent().addClass('active');
+        });
+    });
 });
