3890a31eb1ecb2cffb5a0044cea94d42a4ce3042
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-10-23 18:18:10 +0900

    ~ 241023 18:17

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
     $('.hr_util > a').each(function(){
-        $(this).on('click', function(){
+        $(this).on('click', function(e){
+            e.preventDefault();
             $(this).parent().addClass('active');
+        });
+    });
+
+    $('.hub_close button').each(function(){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $(this).closest('.hr_util').removeClass('active');
         });
     });
 });
