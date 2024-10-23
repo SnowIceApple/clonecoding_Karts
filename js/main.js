b5f912b66be54969cf7c8dbe9d5b531ac4c79194
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-10-24 03:39:35 +0900

    전체 검색창, 전체메뉴 pc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 $(document).ready(function(){
+    AOS.init();
     $('.hr_util > a').each(function(){
         $(this).on('click', function(e){
             e.preventDefault();
             $(this).parent().addClass('active');
         });
+    });
+
+    $('.hr_search > a').on('click',function(e){
+        e.preventDefault();
+        setTimeout(() => {
+            $('#all_search_input').focus();
+        }, 500);
+    });
+
+    var allSearchInput = $('#all_search_input');
+    var allSearchBtn = $('.search_area button');
+
+    allSearchInput.on('keyup', function(){
+        var wordLength = $(this).val();
+
+        if(wordLength.length >= 6){
+            allSearchInput.addClass('wide');
+            allSearchInput.attr('maxlength', 50);
+            allSearchInput.css('text-align', 'center');
+        }
+        else{
+            allSearchInput.removeClass('wide');
+            allSearchInput.attr('maxlength', 12);
+            allSearchInput.css('text-align', 'left');
+        }
+    });
+
+    allSearchBtn.on('click', function(){
+        var wordLength = allSearchInput.val();
+        if(wordLength.length < 2){
+            alert('Please input at least 2 Characters');
+        }
     });
 
     $('.hub_close button').each(function(){
